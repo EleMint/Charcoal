662b75df703b800356b4ac50cd36f7f577aab254
EleMint <dylanschmidt0216@gmail.com>
2018-08-13 09:09:21 -0500

    Added folder for server

---
+++ Charcoal-Server/main.js
@@ -0,0 +1,21 @@
+const app = require('express');
+const http = require('http').Server(app);
+const io = require('socket.io').apply(http);
+const path = require('path');
+const url = require('url');
+
+//app.get('/', function(request, responce){
+//    responce.sendFile(__dirname, 'index.html');
+//});
+
+io.on('connection', function(socket){
+    console.log('A user has connected.');
+});
+
+io.on('disconnection', function(socket){
+    console.log('A user has disconnected.');
+});
+
+http.listen(3000, function(){
+    console.log('Listening on *:3000.');
+});
