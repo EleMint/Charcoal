e01e679dd8b044c09c228b6c489e425a9ca8624c
EleMint <dylanschmidt0216@gmail.com>
2018-08-14 10:48:26 -0500

    SSH server working

--- Charcoal-Client/resources/app/js/sketch.js
+++ Charcoal-Client/resources/app/js/sketch.js
@@ -1,5 +1,12 @@
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
+const socket = io('http://142.93.200.15:3000'); // SSH Server
+//const socket = io('http://192.168.0.128:3000'); // Local Server
+
+
+socket.on('connect', function(socekt){
+    console.log('Connected');
+});
+
 
 // Incoming Message
 socket.on('message', function(data){
--- Charcoal-Server/main.js
+++ Charcoal-Server/main.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
-var server = app.listen(3000);
+var server = app.listen(3000, '192.168.0.128');
 const socket = require('socket.io');
-var io = socket(server);
 
 app.use(express.static('public'));
 
-console.log('Server is now running...');
+console.log('Server is now running....');
 
 io.sockets.on('connection', function(socket){
     console.log(socket.id);
-    io.sockets.on('message', gotMessage);
+    socket.on('message', gotMessage);
 });
 
 function gotMessage(message){
